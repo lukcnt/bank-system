21e01f1180b7b344c053cbb46f35584d557433d0
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2022-12-22 23:30:30 -0300

    A to do list to follow through

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+/* To do
+  1 - Create the system login
+  2 - Create the system menu
+  3 - Create the system classes and functionalities
+  4 - Use POO
+  5 - Use SOLID
+  6 - Use Interfaces
+*/
