1e23e94ca3006cec1aaafb559206b185504a460b
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2022-12-25 21:52:01 -0300

    Implement the login screen base

--- Program.cs
+++ Program.cs
@@ -1,31 +1,50 @@
-﻿int choice;
-
-Console.WriteLine("Welcome to Cantoni Bank!\n"+
-                  "Choose one option below:\n"+
-                  "1 - Login\n"+
-                  "2 - Create an account\n"+
-                  "3 - Exit");
-
-choice = Convert.ToInt32(Console.ReadLine());
-
-if (choice == 1)
+﻿internal class Program
 {
-  // Send to Login screen
-}
-else if (choice == 2)
-{
-  // Send to new account screen
-}
-else
-{
-  // Exit the program
+    public static void Login()
+    {
+      string? id;
+      string? password;
+
+      Console.WriteLine("Enter the CPF or CNPJ:");
+      id = Console.ReadLine();
+
+      Console.WriteLine("Enter the password:");
+      password = Console.ReadLine();
+    }
+
+    private static void Main(string[] args)
+    {
+        int choice;
+
+        Console.WriteLine("Welcome to Cantoni Bank!\n" +
+                          "Choose one option below:\n" +
+                          "1 - Login\n" +
+                          "2 - Create an account\n" +
+                          "3 - Exit");
+
+        choice = Convert.ToInt32(Console.ReadLine());
+
+        if (choice == 1)
+        {
+            // Send to Login screen
+            Login();
+        }
+        else if (choice == 2)
+        {
+            // Send to new account screen
+        }
+        else
+        {
+            // Exit the program
+        }
+    }
 }
 
 /* To do
-  1 - Create the system login
-  2 - Create the system menu
-  3 - Create the system classes and functionalities
-  4 - Use POO
-  5 - Use SOLID
-  6 - Use Interfaces
+ 1 - Create the system login
+ 2 - Create the system menu
+ 3 - Create the system classes and functionalities
+ 4 - Use POO
+ 5 - Use SOLID
+ 6 - Use Interfaces
 */
