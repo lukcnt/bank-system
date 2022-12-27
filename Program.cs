10b0b1d0139bdd0b9ba9cb00fbcfc57d4369b67a
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2022-12-26 23:33:35 -0300

    Remove the to do list comment on the program cs

--- Program.cs
+++ Program.cs
@@ -63,12 +63,3 @@
       } while (systemOn);
     }
 }
-
-/* To do
- 1 - Create the system login
- 2 - Create the system menu
- 3 - Create the system classes and functionalities
- 4 - Use POO
- 5 - Use SOLID
- 6 - Use Interfaces
-*/
