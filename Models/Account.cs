eb7d36aaaf4ad8c8d05ca02c9429e3c2346539e7
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2022-12-27 23:52:25 -0300

    Add the properties needed on the account class

--- Models/Account.cs
+++ Models/Account.cs
@@ -7,6 +7,11 @@ namespace bank_system.Models
 {
     public class Account
     {
-        
+        public string Name { get; set; }
+        public string Identification { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string Pin { get; set; }
     }
 }
