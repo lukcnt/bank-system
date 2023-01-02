d85de2a1f7f500007f0afa921d5fdbb48e676dc6
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-02 16:27:57 -0300

    Add the properties necessary for the Transaction Class

--- bank-system-api/Models/Transaction.cs
+++ bank-system-api/Models/Transaction.cs
@@ -1,6 +1,36 @@
-﻿namespace bank_system_api.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Transactions;
+
+namespace bank_system_api.Models
 {
+    [Table("Transactions")]
     public class Transaction
     {
+        [Key]
+        public int Id { get; set; }
+        public string TransactionUniqueReference { get; set; }
+        public decimal TransactionAmount { get; set; }
+        public TransactionStatus TransactionStatus { get; set; }
+        public bool IsSuccessful => TransactionStatus.Equals(TransactionStatus.Success);
+        public string TransactionSourceAccount { get; set; }
+        public string TransactionDestinationAccount { get; set; }
+        public string TransactionParticulars { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+
+    public enum TransactionStatus
+    {
+        Failed,
+        Success,
+        Error
+    }
+
+    public enum TransactionType
+    {
+        Deposit,
+        Withdrawal,
+        Transfer
     }
 }
