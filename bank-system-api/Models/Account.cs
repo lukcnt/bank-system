5c704fb417b283a705b917accab18ca56c10e921
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-02 16:11:53 -0300

    Add new properties and a construct for the class

--- bank-system-api/Models/Account.cs
+++ bank-system-api/Models/Account.cs
@@ -1,9 +1,13 @@
-﻿using System.Security.Principal;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
 
 namespace bank_system_api.Models
 {
+    [Table("Accounts")]
     public class Account
     {
+        [Key]
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -12,5 +16,26 @@ namespace bank_system_api.Models
         public string Email { get; set; }
         public decimal CurrentAccountBalance { get; set; }
         public AccountType AccountType { get; set; }
+        public string AccountNumberGenerated { get; set; }
+
+        public byte[] PinHash { get; set; }
+        public byte[] PinSalt { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateLastUpdated { get; set; }
+
+        Random rand = new Random();
+        public Account()
+        {
+            AccountNumberGenerated = Convert.ToString((long) rand.NextDouble() * 9_000_000_000L + 1_000_000_000L);
+            AccountName = $"{FirstName} {LastName}";
+        }
+    }
+
+    public enum AccountType
+    {
+        Savings,
+        Current,
+        Corporate,
+        Government
     }
 }
