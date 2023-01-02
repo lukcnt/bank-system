d6a6d080d4e9aa7e9a4db5866a57148597e65546
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-02 15:50:45 -0300

    Add the properties on the Account Class

--- bank-system-api/Models/Account.cs
+++ bank-system-api/Models/Account.cs
@@ -1,6 +1,16 @@
-﻿namespace bank_system_api.Models
+﻿using System.Security.Principal;
+
+namespace bank_system_api.Models
 {
     public class Account
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AccountName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public decimal CurrentAccountBalance { get; set; }
+        public AccountType AccountType { get; set; }
     }
 }
