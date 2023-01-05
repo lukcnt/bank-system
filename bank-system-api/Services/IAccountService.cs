ae6b6d307a6c0a79b4ac839df9c36e20741f9df6
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-05 14:55:34 -0300

    Create the interface for the account service

---
+++ bank-system-api/Services/IAccountService.cs
@@ -0,0 +1,19 @@
+﻿using bank_system_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bank_system_api.Services
+{
+    public interface IAccountService
+    {
+        Account Authenticate(string AccountNumber, string Pin);
+        IEnumerable<Account> GetAllAccounts();
+        Account Create(Account account, string Pin, string ConfirmPin);
+        void Update(Account account, string Pin = null);
+        void Delete(int Id);
+        Account GetById(int Id);
+        Account GetByAccountNumber(string AccountNumber);
+    }
+}
