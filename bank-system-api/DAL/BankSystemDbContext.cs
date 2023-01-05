f8028219bf4465f0dbbf6c2006c149e2cf54025b
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-05 13:24:48 -0300

    Create the DbContext for the API

---
+++ bank-system-api/DAL/BankSystemDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using bank_system_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bank_system_api.DAL
+{
+    public class BankSystemDbContext
+    {
+        public class MyDbContext : DbContext
+        {
+            public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+            {
+
+            }
+
+            public DbSet<Account> Accounts { get; set; }
+            public DbSet<Transaction> Transactions { get; set; }
+        }
+    }
+}
