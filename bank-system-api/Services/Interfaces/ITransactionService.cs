28f2580d98200bb6c051059c51956efbf0bbad89
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-05 15:00:43 -0300

    Create the interface of transaction service

---
+++ bank-system-api/Services/Interfaces/ITransactionService.cs
@@ -0,0 +1,17 @@
+﻿using bank_system_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bank_system_api.Services.Interfaces
+{
+    public interface ITransactionService
+    {
+        Response CreateNewTransaction(Transaction transaction);
+        Response FindTransactionByDate(DateTime date);
+        Response MakeDeposit(string AccountNumber, decimal Amount, string TransactionPin);
+        Response MakeWithdrawal(string AccountNumber, decimal Amount, string TransactionPin);
+        Response MakeFundsTransfer(string FromAccount, string ToAccount, decimal Amount, string TransactionPin);
+    }
+}
