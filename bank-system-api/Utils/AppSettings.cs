9ac149d346565b2e410759216437dede7c4be965
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-06 06:05:02 -0300

    Add app settings

---
+++ bank-system-api/Utils/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bank_system_api.Utils
+{
+    public class AppSettings
+    {
+        public string OurBankSettlementAccount { get; set; }
+    }
+}
