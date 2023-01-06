44f9a718f10ec58d619359f2698552068a89ced3
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-06 06:34:36 -0300

    Create the transaction controller

---
+++ bank-system-api/Controllers/TransactionsController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using bank_system_api.Models;
+using bank_system_api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace bank_system_api.Controllers
+{
+    [ApiController]
+    [Route("api/v3/[controller]")]
+    public class TransactionsController : ControllerBase
+    {
+        private ITransactionService _transactionService;
+        IMapper _mapper;
+
+        public TransactionsController(ITransactionService transactionService, IMapper mapper)
+        {
+            _transactionService = transactionService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("create_new_transaction")]
+        public IActionResult CreateNewTransaction([FromBody] TransactionRequestDto transactionRequest)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(transactionRequest);
+
+            var transaction = _mapper.Map<Transaction>(transactionRequest);
+            return Ok(_transactionService.CreateNewTransaction(transaction));
+        }
+
+        [HttpPost]
+        [Route("make_deposit")]
+        public IActionResult MakeDeposit(string AccountNumber, decimal Amount, string TransactionPin)
+        {
+            if (!Regex.IsMatch(AccountNumber, @"^[0][1-9]\d{9}$|^[1-9]\d{9}$"))
+                return BadRequest("Account number must be 10 digits");
+
+            return Ok(_transactionService.MakeDeposit(AccountNumber, Amount, TransactionPin));
+        }
+
+        [HttpPost]
+        [Route("make_withdrawal")]
+        public IActionResult MakeWithdrawal(string AccountNumber, decimal Amount, string TransactionPin)
+        {
+            if (!Regex.IsMatch(AccountNumber, @"^[0][1-9]\d{9}$|^[1-9]\d{9}$"))
+                return BadRequest("Account number must be 10 digits");
+
+            return Ok(_transactionService.MakeWithdrawal(AccountNumber, Amount, TransactionPin));
+        }
+
+        [HttpPost]
+        [Route("make_funds_transfer")]
+        public IActionResult MakeFundsTransfer(string FromAccount, string ToAccount, decimal Amount, string TransactionPin)
+        {
+            if (!Regex.IsMatch(FromAccount, @"^[0][1-9]\d{9}$|^[1-9]\d{9}$") || !Regex.IsMatch(ToAccount, @"^[0][1-9]\d{9}$|^[1-9]\d{9}$"))
+                return BadRequest("Account number must be 10 digits");
+
+            return Ok(_transactionService.MakeFundsTransfer(FromAccount, ToAccount, Amount, TransactionPin));
+        }
+
+    }
+}
