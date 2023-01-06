d2dd0baa6d570aca0540e8572281173e5e86671c
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-06 05:48:04 -0300

    Create a new migration to fix previous errors

--- bank-system-api/Migrations/20230106083928_updateMigration.Designer.cs
+++ bank-system-api/Migrations/20230106083928_updateMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using bank_system_api.DAL;
 
 namespace bank_system_api.Migrations
 {
     [DbContext(typeof(BankSystemDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230106083928_updateMigration")]
+    partial class updateMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ bank-system-api/Migrations/20230106083928_updateMigration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace bank_system_api.Migrations
+{
+    public partial class updateMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AccountName",
+                table: "Accounts",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AccountType",
+                table: "Accounts",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountName",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "AccountType",
+                table: "Accounts");
+        }
+    }
+}
