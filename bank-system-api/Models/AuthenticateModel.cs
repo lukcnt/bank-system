2c15419a2d64341480c24204ebf9280ddc7cd520
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-06 03:31:11 -0300

    Create the authentication model

---
+++ bank-system-api/Models/AuthenticateModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bank_system_api.Models
+{
+    public class AuthenticateModel
+    {
+        [Required]
+        [RegularExpression(@"^[0][1-9]\d{9}$|^[1-9]\d{0}$")]
+        public string AccountNumber { get; set; }
+        [Required]
+
+        public string Pin { get; set; }
+    }
+}
