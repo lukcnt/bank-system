8bdd9fb122254c64cb58b49f80da4f2721ba4334
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-06 03:31:31 -0300

    Create the controller needed

---
+++ bank-system-api/Controllers/AccountsController.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using bank_system_api.Models;
+using bank_system_api.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace bank_system_api.Controllers
+{
+    [ApiController]
+    [Route("api/v3/[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        private IAccountService _accountService;
+        IMapper _mapper;
+
+        public AccountsController(IAccountService accountService, IMapper mapper)
+        {
+            _accountService= accountService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("register_new_account")]
+        public IActionResult RegisterNewAccount([FromBody] RegisterNewAccountModel newAccount)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(newAccount);
+
+            var account = _mapper.Map<Account>(newAccount);
+            return Ok(_accountService.Create(account, newAccount.Pin, newAccount.ConfirmPin));
+        }
+
+        [HttpGet]
+        [Route("get_all_accounts")]
+        public IActionResult GetAllAccounts()
+        {
+            var accounts = _accountService.GetAllAccounts();
+            var cleanedAccounts = _mapper.Map<IList<GetAccountModel>>(accounts);
+            return Ok(cleanedAccounts);
+        }
+
+        [HttpPost]
+        [Route("authenticate")]
+        public IActionResult Authenticate([FromBody] AuthenticateModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(model);
+
+            return Ok(_accountService.Authenticate(model.AccountNumber, model.Pin));
+        }
+
+        [HttpGet]
+        [Route("get_by_account_number")]
+        public IActionResult GetByAccountNumber(string AccountNumber)
+        {
+            if (!Regex.IsMatch(AccountNumber, @"^[0][1-9]\d{9}$|^[1-9]\d{9}$"))
+                return BadRequest("Account number must be 10 digit");
+
+            var account = _accountService.GetByAccountNumber(AccountNumber);
+            var cleanedAccount = _mapper.Map<GetAccountModel>(account);
+            return Ok(cleanedAccount);
+        }
+
+        [HttpGet]
+        [Route("get_by_account_id")]
+        public IActionResult GetById(int Id)
+        {
+            var account = _accountService.GetById(Id);
+            var cleanedAccount = _mapper.Map<GetAccountModel>(account);
+            return Ok(cleanedAccount);
+        }
+
+        [HttpPost]
+        [Route("update_account")]
+        public IActionResult UpdateAccount([FromBody] UpdateAccountModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(model);
+
+            var account = _mapper.Map<Account>(model);
+
+            _accountService.Update(account, model.Pin);
+            return Ok();
+        }
+    }
+}
