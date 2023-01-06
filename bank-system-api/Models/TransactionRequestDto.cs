e1c5126f4f0e45f078ee655fca489f7776c421a1
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-06 06:17:13 -0300

    Create the transaction request dto

---
+++ bank-system-api/Models/TransactionRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bank_system_api.Models
+{
+    public class TransactionRequestDto
+    {
+        public decimal TransactionAmount { get; set; }
+        public string TransactionSourceAccount { get; set; }
+        public string TransactionDestinationAccount { get; set; }
+        public string TransactionParticulars { get; set; }
+        public TransactionType TransactionType { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
